c02b60d60610280e851040ef34fc7ca518668976
Living-Wizard <itskirkiplaidyoutube@hotmail.com>
2015-06-27 20:25:44 -0400

    Updated Build.Gradle and added the Main Mod Class

---
+++ src/main/java/com/kirkiplaid/ShardsOfPower/ShardsOfPower.java
@@ -0,0 +1,33 @@
+package com.kirkiplaid.ShardsOfPower;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Kirkiplaid on 6/27/2015.
+ */
+
+@Mod(modid = "ShardsOfPower", version = "1.7.10-1.0", name = "Shards Of Power")
+
+public class ShardsOfPower {
+
+    @Mod.Instance("ShardsOfPower")
+    public static ShardsOfPower instance;
+
+    @Mod.EventHandler
+    public void preInitEvent(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void InitEvent(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInitEvent(FMLPostInitializationEvent event) {
+
+    }
+}
